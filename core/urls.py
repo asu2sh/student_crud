6383d5b66a6ad9a2c61f8f1d687c4a5c243fa20d
asu2sh <skull2candy4u@gmail.com>
2023-11-26 22:29:19 +0530

    upload: code

--- core/urls.py
+++ core/urls.py
@@ -16,7 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from enroll import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.home, name='home'),
+    path('save/', views.save_data, name='save'),
+    path('delete/', views.delete_data, name='delete'),
+    path('edit/', views.edit_data, name='edit'),
 ]
--- enroll/views.py
+++ enroll/views.py
@@ -1,3 +1,48 @@
 from django.shortcuts import render
+from .forms import StudentRegistration
+from .models import Student
+from django.http import JsonResponse
 
-# Create your views here.
+
+def home(request):
+    fm = StudentRegistration()
+    stud = Student.objects.all()
+    return render(request, 'enroll/home.html', {'form': fm, 'students': stud})
+
+
+def save_data(request):
+    if request.method == "POST":
+        fm = StudentRegistration(request.POST)
+        if fm.is_valid():
+            name = request.POST['name']
+            email = request.POST['email']
+            password = request.POST['password']
+            sid = request.POST.get('sid')
+            if sid == '':
+                stud = Student(name=name, email=email, password=password)
+            else:
+                stud = Student(name=name, email=email, password=password, id=sid)
+            stud.save()
+            students = Student.objects.values()
+            student_data = list(students)
+            return JsonResponse({'student_data': student_data, 'status': 1})
+        else:
+            return JsonResponse({'status': 0})
+
+def delete_data(request):
+    if request.method == "POST":
+        id = request.POST.get('sid')
+        stud = Student.objects.get(pk=id)
+        stud.delete()
+        return JsonResponse({'status': 1})
+    else:
+        return JsonResponse({'status': 0})
+    
+def edit_data(request):
+    if request.method == "POST":
+        id = request.POST.get('sid')
+        stud = Student.objects.get(pk=id)
+        student_data = {"id": stud.id, "name": stud.name, "email": stud.email, "password": stud.password}
+        return JsonResponse(student_data)
+    else:
+        return JsonResponse({'status': 0})
